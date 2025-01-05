c7ebd93ca44a4157a33f5bdfc19305067cb78e59
Lakshmi <nrlakshmi8@gmail.com>
2025-01-05 11:22:52 +0530

    Escape Sequence concept file added

---
+++ src/ex_05012025/Lab_009.py
@@ -0,0 +1,18 @@
+#Escape Sequence
+
+print("Hello World")
+print("Hello\nWorld") # New Line
+print("Hello\tWorld") # Tab Space
+print("Hello\bWorld") # Back Space
+
+# dir = "c:\Lakshmi\n.txt"
+# dir = 'c:\Lakshmi\n.txt'
+
+# Where this r concept used?
+# Automation API, Web Automation, file_path = r concept
+
+dir = r"c:\Lakshmi\n.txt"
+dir2 = r'c:\Lakshmi\n.txt'
+print(dir)
+print(dir2)
+
